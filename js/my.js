1771952ce3093856ffda355788aa5d964589cd17
Kevin Castillo <kcastillo@innovationssystems.com>
2013-11-08 11:39:52 -0500

    aa

--- js/my.js
+++ js/my.js
@@ -231,7 +231,7 @@ $(document).ready(function() {
 });
 
 $(document).addEventListener("backbutton", function(e){
-    if($.mobile.activePage.is('#homepage')){
+    if($.mobile.activePage.is('#page14')){
         e.preventDefault();
         navigator.app.exitApp();
     }
